dc953d20877c80cb3071233095d43b0986f5b120
dsoftware763 <dsoftware763@gmail.com>
2020-03-02 17:30:41 +0530

    Replace url

--- frontend/src/state/auth/queries.js
+++ frontend/src/state/auth/queries.js
@@ -1,43 +1,43 @@
 import axios from 'axios'
 
 export const loginApi = (credentials) => {
-  return axios.post('http://localhost:2018/public/login', { ...credentials })
+  return axios.post('/public/login', { ...credentials })
     .then(res => res.data)
     .catch(err => err.response.data)
 };
 
 export const registerApi = (credentials) => {
-  return axios.post('http://localhost:2018/public/register', { ...credentials })
+  return axios.post('/public/register', { ...credentials })
     .then(res => res.data)
     .catch(err => err.response.data)
 };
 
 export const updateApi = (credentials) => {
-  return axios.put('http://localhost:2018/private/users', { ...credentials }, { headers: { Authorization: "Bearer " + localStorage.getItem('token') } })
+  return axios.put('/private/users', { ...credentials }, { headers: { Authorization: "Bearer " + localStorage.getItem('token') } })
     .then(res => res.data)
     .catch(err =>  err.response.data)
 };
 
 export const validateApi = (token) => {
-  return axios.post('http://localhost:2018/public/validate', { token })
+  return axios.post('/public/validate', { token })
     .then(res => res.data)
     .catch(err =>  err.response.data )
 };
 
 export const recoverPasswordApi = (credentials) => {
-  return axios.post('http://localhost:2018/public/recover-password', { ...credentials })
+  return axios.post('/public/recover-password', { ...credentials })
     .then(res => res.data)
     .catch(err => err.response.data )
 };
 
 export const resetPasswordApi = (credentials) => {
-  return axios.post('http://localhost:2018/public/reset-password', { ...credentials })
+  return axios.post('/public/reset-password', { ...credentials })
     .then(res => res.data)
     .catch(err => err.response.data)
 };
 
 export const toggleSubscriptionApi = (value, token) => {
-  return axios.post('http://localhost:2018/private/toggle-subscription', { value }, { headers: { Authorization: "Bearer " + token } })
+  return axios.post('/private/toggle-subscription', { value }, { headers: { Authorization: "Bearer " + token } })
     .then(res => res.data)
     .catch(err => err.response.data)
 };
